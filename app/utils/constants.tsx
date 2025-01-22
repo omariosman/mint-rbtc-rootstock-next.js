c7e7797fc820c9a170050678fcdaaff93bd87604
omariosman <omarosman23@aucegypt.edu>
2025-01-22 18:09:53 +0200

    :tada: first commit

---
+++ app/utils/constants.tsx
@@ -0,0 +1,8 @@
+export const RSK_TESTNET_NODES = [
+    "https://public-node.testnet.rsk.co",
+    "https://testnet.rootsotck.io/rpc"
+]
+
+export const RSK_MAINNET_NODES = [
+    "https://public-node.rsk.co",
+]
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
 };
 
 export default nextConfig;
